f515823cb060eba29fd6bd3c0811a0caeabbd7f2
Elliott Larsen <elliottlrsn@gmail.com>
2024-05-11 14:40:21 -0600

    Another GET endpoint added.

--- MVC/src/main/java/com/example/MVC/controller/FirstController.java
+++ MVC/src/main/java/com/example/MVC/controller/FirstController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
+    String username = "John Doe";
     @GetMapping("/greeting")
     public String greeting(Model model) {
-        model.addAttribute("username", "!");
+        model.addAttribute("username", username);
         return "greetings";
+    }
+
+    @GetMapping("/goodbye")
+    public String farewell(Model model) {
+        model.addAttribute("username", username);
+        return "farewell";
     }
 }
