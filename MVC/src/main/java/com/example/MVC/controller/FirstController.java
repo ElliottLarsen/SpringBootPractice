12348d633f51081536c6b08fe4e9529cef2b6db7
Elliott Larsen <elliottlrsn@gmail.com>
2024-05-11 14:00:29 -0600

    Model added.

--- MVC/src/main/java/com/example/MVC/controller/FirstController.java
+++ MVC/src/main/java/com/example/MVC/controller/FirstController.java
@@ -1,12 +1,14 @@
 package com.example.MVC.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/greeting")
-    public String greeting() {
+    public String greeting(Model model) {
+        model.addAttribute("username", "!");
         return "greetings";
     }
 }
